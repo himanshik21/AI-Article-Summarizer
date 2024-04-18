95afa3e1d05823c36e7678ce6a0e088a998446ae
Himanshi Khandelwal <himanshikhandelwal81@gmail.com>
2024-04-18 21:54:56 +0530

    First Commit

--- src/services/articleApi.js
+++ src/services/articleApi.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-const rapidApikey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+const rapidApikey = import.meta.env.VITE_RAPID_API_ARTICLE_SUMMARIZER_KEY;
 
 export const articleApi = createApi({
     reducerPath: 'articleApi',
